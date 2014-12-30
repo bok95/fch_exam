08c5f73fb531c1d513aa0157668518993195f18d
windwishers <shadowog@gmail.com>
2014-12-30 23:39:42 +0900

    facebook chat head

--- app/src/main/java/fch/epic/fail/facebookchathead/ChatHeadService.java
+++ app/src/main/java/fch/epic/fail/facebookchathead/ChatHeadService.java
@@ -2,15 +2,48 @@ package fch.epic.fail.facebookchathead;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.IBinder;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class ChatHeadService extends Service {
-    public ChatHeadService() {
+
+    private WindowManager windowManager;
+    private ImageView chatHead;
+
+    @Override public IBinder onBind(Intent intent) {
+        // Not used
+        return null;
+    }
+
+    @Override public void onCreate() {
+        super.onCreate();
+
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        chatHead = new ImageView(this);
+        chatHead.setImageResource(R.drawable.ic_launcher);
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.TOP | Gravity.LEFT;
+        params.x = 0;
+        params.y = 100;
+        //params.width
+        //params.height
+        windowManager.addView(chatHead, params);
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public void onDestroy() {
+        super.onDestroy();
+        if (chatHead != null) windowManager.removeView(chatHead);
     }
 }
--- app/src/main/java/fch/epic/fail/facebookchathead/ControledActivity.java
+++ app/src/main/java/fch/epic/fail/facebookchathead/ControledActivity.java
@@ -1,17 +1,34 @@
 package fch.epic.fail.facebookchathead;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class ControledActivity extends ActionBarActivity {
+
+    private Button button_fch;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controled);
+        button_fch = (Button) findViewById(R.id.button_start_fch);
+        mContext = this;
+        button_fch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startService(new Intent(mContext, ChatHeadService.class));
+            }
+        });
+
     }
 
 
