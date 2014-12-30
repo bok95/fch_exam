e2b28c1376078c949cb4f7f167de951e14c4b645
windwishers <shadowog@gmail.com>
2014-12-30 23:56:39 +0900

    add drag in head

--- app/src/main/java/fch/epic/fail/facebookchathead/ChatHeadService.java
+++ app/src/main/java/fch/epic/fail/facebookchathead/ChatHeadService.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
@@ -26,7 +28,7 @@ public class ChatHeadService extends Service {
         chatHead = new ImageView(this);
         chatHead.setImageResource(R.drawable.ic_launcher);
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_PHONE,
@@ -39,6 +41,34 @@ public class ChatHeadService extends Service {
         //params.width
         //params.height
         windowManager.addView(chatHead, params);
+
+
+        chatHead.setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+            @Override public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        initialX = params.x;
+                        initialY = params.y;
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+                        windowManager.updateViewLayout(chatHead, params);
+                        return true;
+                }
+                return false;
+            }
+        });
+
     }
 
     @Override
